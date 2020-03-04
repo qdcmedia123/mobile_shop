f3915cd7229c31e330f3c357e050131a95e3d0e0
Ron <akashche72@gmail.com>
2020-03-04 22:19:16 +0400

    Secound commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React , { useState }from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+import cartReducer from './store/reducers/cart';
+import OrdersReducer from './store/reducers/orders';
+
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: OrdersReducer
+
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+  );
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  
+  })
+}
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  
+  if(!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync = {fetchFonts}
+        onFinish = {() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
+}
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Platform, Text } from 'react-native';
+import Colors from '../constants/Colors';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+
+import OrdersScreen from '../screens/shop/OrdersScreen';
+
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import { Ionicons } from '@expo/vector-icons';
+import OrderScreen from '../screens/shop/OrdersScreen';
+import UserProudctsScreen from '../screens/user/UserProductsScreen';
+
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+    fontFamily: 'open-sans-bold'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+};
+
+
+const ProductsNavigator = createStackNavigator(
+    {
+      ProductsOverview: ProductsOverviewScreen,
+      ProductDetail: ProductDetailScreen,
+      Cart: CartScreen
+    },
+    {
+      navigationOptions: {
+        drawerIcon: drawerConfig => <Ionicons name = { Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+        size= {23}
+        color = {drawerConfig.activeTintColor}
+        />
+      },
+
+      defaultNavigationOptions: defaultNavOptions
+    }
+  );
+
+
+const AdminNavigator = createStackNavigator(
+  {
+  UserProducs: UserProudctsScreen,
+  },
+  {
+    navigationOptions: {
+      drawerIcon: drawerConfig => <Ionicons
+       name = {Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+       size = {23}
+       color = {drawerConfig.activeTintColor}
+      />
+    },
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
+const OrdersNavigator = createStackNavigator(
+  {
+  Orders: OrderScreen,
+  },
+  {
+    navigationOptions: {
+      drawerIcon: drawerConfig => <Ionicons
+       name = {Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+       size = {23}
+       color = {drawerConfig.activeTintColor}
+      />
+    },
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
+
+
+const ShopNavigator = createDrawerNavigator(
+  {Products: ProductsNavigator,
+  Orders: OrdersNavigator,
+  Admin: AdminNavigator
+}, 
+{
+  contentOption: {
+
+  }
+}
+  )
+  
+  
+  export default createAppContainer(ShopNavigator);
